4ff114adab493e4b3613cf86850498b3e8bdf069
John Chen <cp_china@163.com>
2017-11-01 11:05:01 +0800

    Add python demos for data types and variables.

---
+++ basic_functions/datatypes_and_variables.py
@@ -0,0 +1,92 @@
+# name = input('please enter your name: ')
+# print('Hello,', name)
+a = 100
+if a >= 0:
+    print(a)
+else:
+    print(-a)
+
+
+print 'abc'
+print 'a b c'
+print "I'm OK"
+print (1.23e10)
+print 'I\'m \"OK\"'
+
+print ('I\'m OK')
+print ('abc')
+print ('I\'m learning\nPython')
+
+print ('\\\n\\')
+print ('\\\t\\')
+print (r'\\\t\\')
+
+print ('''line1
+...line2
+...line3''')
+
+print ('''line1
+line2
+line3''')
+
+print True
+print False
+print True and False
+print True or False
+print 5 > 3 and 1 > 3
+print 5 > 3 or 1 > 3
+
+print not True
+print not False
+print not 1 > 2
+print not 2 > 1
+
+age = 18
+if age >= 18:
+    print ('adult')
+else:
+    print ('teenager')
+
+print ('__')
+
+if age >= 18:
+    print ('OK')
+else:
+    print ('Bad')
+
+a = 123
+print (a)
+
+t_007 = 'T007'
+print (t_007)
+
+Answer = True
+print (Answer)
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print ('a = ' + a)
+print ('b = ' + b)
+
+PI = 3.14159265359
+print PI
+
+print 10/3.0
+print 9/3.0
+print 10//3.0
+print 10%3.0
+
+n = 123
+print n
+f =456.789
+print f
+s1 = 'Hello, world'
+print s1
+s2 = 'Hello, \'Adam\''
+print s2
+s3 = r'Hello, "Bart"'
+print s3
+s4 = r'''Hello,
+Lisa!'''
+print s4
