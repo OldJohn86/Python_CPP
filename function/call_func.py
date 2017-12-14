4d10979725944588023863a6867d77e082355e45
John.Chen <johnchen5372@gmail.com>
2017-12-14 15:53:43 +0800

    Add function modules.

---
+++ function/call_func.py
@@ -0,0 +1,27 @@
+print(abs(100))
+print(abs(-20))
+print(abs(12.34))
+
+print(max(1, 2))
+print(max(2, 3, 1, -5))
+
+print(min(1, 2, 3, 4))
+
+print(int('123'))
+print(int(12.34))
+
+print(float('12.34'))
+
+print(str(1.23))
+print(str(100))
+
+print(bool(1))
+print(bool(''))
+
+a = abs
+print(a(-1))
+
+n1 =255
+n2 = 1000
+print(hex(n1))
+print(hex(n2))
