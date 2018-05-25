f99fc3da48a99d433822c52f1c16781e2467c90b
John.Chen <johnchen5372@gmail.com>
2018-05-25 15:13:34 +0800

    update regex.py

--- muti_proc/regex.py
+++ muti_proc/regex.py
@@ -70,7 +70,9 @@ def name_of_email(addr):
 		return r.group(1)
 
 assert name_of_email('<Tom Paris> tom@voyager.org') == 'Tom Paris'
+print(name_of_email('<Tom Paris> tom@voyager.org'))
 assert name_of_email('tom@voyager.org') == 'tom'
+print(name_of_email('tom@voyager.org'))
 print('ok')
 
 
