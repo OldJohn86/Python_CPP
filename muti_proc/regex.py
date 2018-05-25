94a7a9f1f363c04dab0023bfa21483571be2f182
John.Chen <johnchen5372@gmail.com>
2018-05-25 15:07:40 +0800

    Update regex.py

--- muti_proc/regex.py
+++ muti_proc/regex.py
@@ -41,3 +41,37 @@ print(re_telephone.match('010-12345').groups())
 print(re_telephone.match('010-8086').groups())
 
 
+import re
+
+email1 = 'someone@gmail.com'
+email2 = 'bill.gates@microsoft.com'
+
+email_match = re.compile(r'^[a-zA-Z\][0-9a-zA-Z.]+@[0-9a-zA-Z_]+.com$')
+
+def is_valid_email(addr):
+	if email_match.match(addr):
+		return True
+	else:
+		return False
+
+assert is_valid_email('someone@gmail.com')
+assert is_valid_email('bill.gates@microsoft.com')
+assert not is_valid_email('bob#example.com')
+assert not is_valid_email('mr-bob@example.com')
+print('ok')
+
+
+import re
+re_email_name = re.compile(r'^[<]?([\w\s]+)[>|@].+$')
+
+def name_of_email(addr):
+	r = re_email_name.match(addr)
+	if r:
+		return r.group(1)
+
+assert name_of_email('<Tom Paris> tom@voyager.org') == 'Tom Paris'
+assert name_of_email('tom@voyager.org') == 'tom'
+print('ok')
+
+
+
