39eb7b8c9d842776b147858a58571d4d7b26279f
John.Chen <johnchen5372@gmail.com>
2018-07-18 12:01:33 +0800

    Update telnet_exp.py

--- cortina_task/sanity_test/telnet_exp.py
+++ cortina_task/sanity_test/telnet_exp.py
@@ -9,9 +9,16 @@ import time
 HOST = "192.168.41.251"
 PORT = 2012
 
-#with telnetlib.Telnet(HOST, PORT) as tn:
-#    tn.write(b"ls /home/\n")
-#print(tn.read_all().decode('ascii'))
+activeport_set     = b'setenv active_port 1;\r\n'
+serverip_set       = b'setenv serverip 192.168.1.128;\r\n'
+ipaddr_set         = b'setenv ipaddr 192.168.1.2;\r\n'
+saveenv_set        = b'saveenv;\r\n'
+reset_set          = b'reset\r\n'
+
+tftpboot_gpt       = b'tftpboot 0x84100000 major-image-g3-eng-gpt-nandinfo.img; nand erase 0x0 0x400000; nand write 0x84100000 0x0 0x300000;\r\n'
+tftpboot_ubootenv  = b'tftpboot 0x84000000 g3-uboot-env.bin; nand erase 0x400000 0x100000; nand write 0x84000000 0x400000 0x20000;\r\n'
+tftpboot_image     = b'tftpboot 0x85000000 major-image-g3-eng.mubi; nand erase 0x500000 0xD000000; nand write 0x85000000 0x500000 0x${filesize};\r\n'
+
 
 def do_telnet(host):
     tn = telnetlib.Telnet(host, port=2012, timeout=50)
@@ -22,16 +29,47 @@ def do_telnet(host):
     # tn.read_until(b'Password: ')
     # tn.write(password + b'\n')
     
-    time.sleep(1)
     tn.write(b'root\n')
     time.sleep(1)
     tn.write(b'\r\n')
-    tn.write(b'\r\n')
-    tn.write(b'\r\n')
-    tn.read_until(b'root@g3-eng:~# ')
+    time.sleep(1)
 
+    tn.read_until(b'root@g3-eng:~# ')
     tn.write(b'reboot\n')
+    time.sleep(1)
+
+    tn.read_until(b'Hit any key to stop autoboot: ')
+    tn.write(b'\r\n')
+    time.sleep(1)
+
+    tn.write(activeport_set)
+    tn.write(serverip_set)
+    tn.write(ipaddr_set)
+    tn.write(saveenv_set)
+    tn.write(reset_set)
+
+    tn.read_until(b'Hit any key to stop autoboot: ')
+    tn.write(b'\r\n')
+    time.sleep(1)
+
+    # Upgrade gpt
+    tn.read_until(b'G3# ')
+    tn.write(tftpboot_gpt)
+    time.sleep(1)
+    # Upgrade uboot_env
+    tn.read_until(b'written: OK')
+    tn.write(tftpboot_ubootenv)
+    time.sleep(1)
+    # Upgrade image
+    tn.read_until(b'written: OK')
+    tn.write(tftpboot_image)
+    time.sleep(1)
+    # RESET board
+    tn.read_until(b'written: OK')
+    tn.write(reset_set)
+
     time.sleep(50)
+    # result_str = tn.read_all()
     result_str = tn.read_very_eager()
     return (result_str.decode('ascii', errors='ignore'))
 
@@ -43,6 +81,6 @@ if __name__ == '__main__':
     log_txt = do_telnet(host)
     print(log_txt)
 
-    with open('./logfile/g3_santy_test.txt', 'w') as f:
+    with open('./logfile/g3_santy_testlog.txt', 'w') as f:
         f.write(log_txt)
 
