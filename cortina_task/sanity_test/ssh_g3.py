f6c90b2ae4b620aa981d1e8b32d4ffd4af13dff6
John.Chen <johnchen5372@gmail.com>
2018-07-18 15:15:43 +0800

    Update ssh_g3.py

--- cortina_task/sanity_test/ssh_g3.py
+++ cortina_task/sanity_test/ssh_g3.py
@@ -3,8 +3,22 @@
 # -*- coding: utf-8 -*-
 import paramiko
 import os
+from datetime import date
 from configparser import ConfigParser
 
+g3_image_url     = 'http://192.168.65.235:18683/g3/'
+g3_image_type    = 'g3-eng-major-image'
+g3_gpt           = 'major-image-g3-eng-gpt-nandinfo.img'
+ubootenv         = 'uboot-env.bin'
+g3_ubootenv      = 'g3-uboot-env.bin'
+g3_image         = 'major-image-g3-eng.mubi'
+sanity_test_path = 'cd daily_image_sanity_test;'
+
+# day = date.today()
+y_m_d = date.today().strftime('%Y-%m-%d')
+y_m = date.today().strftime('%Y-%m')
+print(y_m_d)
+print(y_m)
 
 # 读取配置文件获取服务器的登录信息
 def read_ini():
@@ -16,7 +30,6 @@ def read_ini():
         info[each] = cf.get('ssh', each)
     print(info)
     return info
-
 
 # 连接服务区并执行shell命令返回输出结果
 def ssh_test(host, port, username, password):
@@ -36,30 +49,41 @@ def ssh_test(host, port, username, password):
         return result.decode().strip()
 
     # 获取指定文件夹的绝对地址
-    cmd_get_path = 'cd daily_image_sanity_test; pwd'
+    cmd_get_path = sanity_test_path +' pwd'
     db_path = run_shell(cmd_get_path)
 
+    # Removed old image files
+    cmd_rm_image = sanity_test_path +' rm *g3*'
+    rm_ret = run_shell(cmd_rm_image)
+
+    # Get the lastest image files from Server IP
+    cmd_wget_g3gpt = sanity_test_path +' wget '+g3_image_url+'/'+y_m+'/'+y_m_d+'/'+g3_image_type+'/'+g3_gpt
+    print(cmd_wget_g3gpt)
+    g3gpt_ret = run_shell(cmd_wget_g3gpt)
+
+    cmd_wget_ubootenv = sanity_test_path +' wget '+g3_image_url+'/'+y_m+'/'+y_m_d+'/'+g3_image_type+'/'+ubootenv
+    print(cmd_wget_ubootenv)
+    ubootenv_ret = run_shell(cmd_wget_ubootenv)
+
+    cmd_rename_g3ubootenv = sanity_test_path + ' mv '+ubootenv+' '+g3_ubootenv
+    print(cmd_rename_g3ubootenv)
+    g3ubootenv_ret = run_shell(cmd_rename_g3ubootenv)
+
+    cmd_wget_g3image = sanity_test_path +' wget '+g3_image_url+'/'+y_m+'/'+y_m_d+'/'+g3_image_type+'/'+g3_image
+    print(cmd_wget_g3image)
+    g3image_ret = run_shell(cmd_wget_g3image)
+
     # 获取指定文件夹中文件的名称，并跟上面得到的文件夹绝对地址组合起来
-    cmd_get_sqls = 'cd daily_image_sanity_test; ls'
+    cmd_get_sqls = sanity_test_path +' ls'
     sqls = run_shell(cmd_get_sqls)
-    print(sqls)
-
-    # 判断是否为子目录
-    cmd_get_dir ='cd daily_image_sanity_test; ls -l|grep ^d|awk \'{print$9}\'' 
-    dir_name = run_shell(cmd_get_dir)
-    print(dir_name)
-
-    # 去除子目录路径
-#sqls = sqls.replace(dir_name + '\n', ' ')
-#    print(sqls)
+    # print(sqls)
 
     lis = ['{}/{}'.format(db_path, each) for each in sqls.split('\n')]
-    print(lis)
+    # print(lis)
 
     # 关闭连接
     ssh.close()
     return lis
-
 
 # 链接服务器进行文件传输
 def sftp_test(host, port, username, password, from_file, to_file):
@@ -83,19 +107,17 @@ if __name__ == '__main__':
     pw = info.get('password', None)
     files = ssh_test(h, p, u, pw)
 
-    path = './bash_scripts'
+    path = './g3_daily_image'
     if os.path.exists(path):
         pass
     else:
-        os.mkdirs(path)
+        os.makedirs(path)
 
-    target = ['expect_scp.sh', 'send_logs.sh', 'wget_image.sh']
-
+    target = [g3_image, g3_gpt, g3_ubootenv]
     if files:
         for each in files:
             name = each.split('/')[-1]
-#print(each)
-#print(name)
             if name in target:
+                print(name)
                 ss = sftp_test(h, p, u, pw, each, os.path.join(path, name))
 
