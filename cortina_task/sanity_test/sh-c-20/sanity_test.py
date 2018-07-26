dd7ecd49afeec9f5fe47c70eec726986bf8eb245
John.Chen <johnchen5372@gmail.com>
2018-07-26 15:38:29 +0800

    Removed unused print()

--- cortina_task/sanity_test/sh-c-20/sanity_test.py
+++ cortina_task/sanity_test/sh-c-20/sanity_test.py
@@ -76,7 +76,7 @@ def download_img(obj, current_path, config, target, child=''):
     path_info = read_ini(config, 'path')
     local_path = path_info.get('local_path', None)
     local_path_abs = os.path.join(current_path, local_path)
-    print(local_path_abs)
+    # print(local_path_abs)
     if not os.path.exists(local_path_abs):
         os.makedirs(local_path_abs)
     else:
@@ -111,11 +111,11 @@ def download_img(obj, current_path, config, target, child=''):
         old_backfile_list = os.listdir(local_backup_path_abs)
         for item in old_backfile_list:
             os.remove(local_backup_path_abs +'/' + item)
-    print(local_backup_path_abs)
+    # print(local_backup_path_abs)
     remote_path = path_info.get('remote_path', None)
     remote_path_abs = remote_path + target+'/'+ y_m +'/'+ y_m_d + target_path
     return_items = getattr(obj, "connect")(remote_path_abs)
-    print(return_items)
+    # print(return_items)
     if target == 'g3':
         log_file = y_m_d +'-'+ target + '-sanitytest-log.txt';
     elif target == 'saturn-sfu':
@@ -142,7 +142,7 @@ def download_img(obj, current_path, config, target, child=''):
                     local_file = os.path.join(local_path_abs, item)
                 local_backup_file = os.path.join(local_backup_path_abs, item)
                 if target != 'g3hgu':
-                    print(local_file)
+                    # print(local_file)
                     getattr(obj, "input")(local_file, remote_file)
                     getattr(obj, "get")()
                     time.sleep(1)
@@ -161,7 +161,7 @@ def upload_log(obj, current_path, config, target, child=''):
     path_info = read_ini(config, 'path')
     local_path = path_info.get('local_path', None)
     local_path_abs = os.path.join(current_path, local_path)
-    print(local_path_abs)
+    # print(local_path_abs)
     if not os.path.exists(local_path_abs):
         print("ERROR: local_path[%s] NOT exists!!" % local_path_abs)
     if target == 'g3':
@@ -180,14 +180,14 @@ def upload_log(obj, current_path, config, target, child=''):
     remote_path = path_info.get('remote_path', None)
     remote_path_abs = remote_path + target+'/'+ y_m +'/'+ y_m_d + target_path
     return_items = getattr(obj, "connect")(remote_path_abs)
-    print(return_items)
+    # print(return_items)
     target_info = read_ini(config, target)
     for item in target_info.values():
         if log_file == item:
             remote_file = remote_path + target +'/'+ y_m +'/'+ y_m_d + target_path + y_m_d +'-'+ item
-            print(remote_file)
+            # print(remote_file)
             local_file = os.path.join(local_path_abs, y_m_d +'-'+ item)
-            print(local_file)
+            # print(local_file)
             if not os.path.exists(local_file):
                 print("ERROR: local_file[%s] NOT exists!!" % local_file)
             getattr(obj, "input")(local_file, remote_file)
@@ -306,7 +306,7 @@ def capture_log(current_path, config, target, child=''):
         log_file_path = local_path_abs + y_m_d +'-'+ child +'-sanitytest-log.txt'; 
     else:
         pass
-    print(log_file_path)
+    # print(log_file_path)
     log_txt = do_telnet(config, target)
     with open(log_file_path, 'w') as f:
         f.write(log_txt)
@@ -339,6 +339,7 @@ if __name__ == "__main__":
             upload_log(obj, current_path, config, 'g3')
             time.sleep(2)
         else:
+            print("g3 sanity test process sleep 20 Minutes")
             time.sleep(20*60) # wait 1/3 hour
 
         # Epon sanity test process
@@ -351,16 +352,18 @@ if __name__ == "__main__":
             upload_log(obj, current_path, config, 'saturn-sfu', 'epon')
             time.sleep(2)
         else:
+            print("saturn-sfu_epon sanity test process sleep 20 Minutes")
             time.sleep(20*60)
 
         # Gpon sanity test process
         gpon_img_ok = download_img(obj, current_path, config, 'saturn-sfu', 'gpon')
         time.sleep(2)
-        print("saturn-sfu_epon img download is %s" % epon_img_ok)
+        print("saturn-sfu_gpon img download is %s" % epon_img_ok)
         if gpon_img_ok == True:
             capture_log(current_path, config, 'saturn-sfu', 'gpon')
             time.sleep(2)
             upload_log(obj, current_path, config, 'saturn-sfu', 'gpon')
         else:
+            print("saturn-sfu_gpon sanity test process sleep 20 Minutes")
             time.sleep(20*60)
     print("--- --- --- G3/EPON/GPON Sanity Test End!!! --- --- ---")
