98ea159c3f734159eb5b1bd53d4b2d50a206e98d
John.Chen <johnchen5372@gmail.com>
2018-09-12 18:57:37 +0800

    Removed data blank Dirictory

--- python_scrap/caiqingcai.com/post_demo.py
+++ python_scrap/caiqingcai.com/post_demo.py
@@ -1,7 +1,7 @@
 import urllib
 import urllib2
 
-values = {"username":"cp_china@163.com","password":"Chen5372"}
+values = {"username":"cp_china@163.com","password":"XXXXXX"}
 data = urllib.urlencode(values)
 url = "https://passport.csdn.net/account/login?from=http://my.csdn.net/my/mycsdn"
 request = urllib2.Request(url,data)
