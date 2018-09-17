e0b53d352f71ca73b6f5028785bbacdc0142f11d
John.Chen <johnchen5372@gmail.com>
2018-09-17 12:45:09 +0800

    Add error process

--- python_scrap/WebScrapingWithPython/chapter1/beautful4.py
+++ python_scrap/WebScrapingWithPython/chapter1/beautful4.py
@@ -1,6 +1,21 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), features="lxml")
-print(bsObj.h1)
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), features="lxml")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
