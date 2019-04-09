6accbab6c9411106ba20ca4023f6ca2a8198d2df
Johnny Chen <johnchen5372@gmail.com>
2019-04-09 13:46:54 +0800

    update NumPy_demo

--- NumPy_demo/demo_02.py
+++ NumPy_demo/demo_02.py
@@ -1,7 +1,7 @@
 #---*** coding=utf-8 ***---
 
-import numpy as np
-import matplotlib.pyplot as plt
+#import numpy as np
+#import matplotlib.pyplot as plt
 
 def quicksort(arr):
     if len(arr) <= 1:
