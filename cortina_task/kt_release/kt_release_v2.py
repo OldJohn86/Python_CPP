841bc2acca9a58b49506021864ff93b9193f6b6d
Johnny Chen <johnchen5372@gmail.com>
2019-04-24 15:54:50 +0800

    Add kt_release_v1.py kt_release_v2.py

--- cortina_task/kt_release/kt_release_v2.py
+++ cortina_task/kt_release/kt_release_v2.py
@@ -19,6 +19,8 @@ build_path = 'cd /space/pchen/kt_release; '
 # print(build_path)
 yocto_path = build_path.replace('; ', '/') +'yocto-'+ datetag+'; '
 # print(yocto_path)
+glb_kt_ver = '1.0.0.9.'
+key_word = "21.00.00.09."
 
 # Read config info
 def read_ini():
@@ -82,6 +84,27 @@ class SSHConnection(object):
 			#print(err.decode('utf8').strip())  #输出错误结果
 			return err.decode('utf8').strip()
 
+	def invoke_shell_command(self, command):
+		if self._client is None:
+			self._client = paramiko.SSHClient()
+			self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+			try:
+				self._client.connect(self._host, self._port, self._username, self._password, timeout=10)
+			except Exception as e:
+				print(e)
+		chan = self._client.invoke_shell()
+		chan.sendall(command +'\n')
+		lines = []
+		while True:
+			line = str(chan.recv(1024))
+			if "Summary: " in line:
+				lines.append(line)
+				break
+			lines.append(line)
+		result = ' '.join(lines)
+		#return result
+		return line.decode('utf8').strip()
+
 	def close(self):
 		if self._transport:
 			self._transport.close()
@@ -106,7 +129,7 @@ def yocto_gitclone(SSHConnection):
 
 #@#$%^&
 def version_autoincrement(_file):
-    key_word = "21.00.00.09."
+    global glb_kt_ver
     file_data = ""
     key_char = []
     with open(_file, "r", encoding="utf-8") as f:
@@ -117,18 +140,18 @@ def version_autoincrement(_file):
                 key_char = cur_ver[-1].split('.')
                 # print(key_char)
                 key_char[-1] = str(int(key_char[-1]) + 1)
-                kt_ver = '1.0.0.9.'+ key_char[-1]
+                glb_kt_ver = '1.0.0.9.' + key_char[-1]
                 key_char[-1] = key_char[-1].zfill(2)
                 # print(key_char)
                 new_ver = ".".join(key_char)
-                #print(new_ver)
+                # print(new_ver)
                 line = line.replace(cur_ver[-1], new_ver)
                 print("NEW_VER: " + line.strip('\n'))
-                print("KT_VER: " + kt_ver)
+                print("KT_VER: " + glb_kt_ver)
             file_data += line
     with open(_file, "w", encoding="utf-8") as f:
         f.write(file_data)
-		
+
 # @#$%^ define modify release verion func
 def update_version(SSHConnection):
     if not os.path.exists('./tmp'):
@@ -145,12 +168,37 @@ def update_version(SSHConnection):
     ]
     for index, ff in enumerate(from_file):
         localfile = to_file[index]
-        print(localfile)
+        # print(localfile)
         remotefile = yocto_path.split()[-1].replace(';', '/') + ff
         SSHConnection.download(remotefile, localfile)
         version_autoincrement(localfile)
         SSHConnection.put(localfile, remotefile)
         print(remotefile)
+		
+def build_image(SSHConnection):
+	cmd_bitbake = 'TEMPLATECONF=meta-oe-ca/conf/saturn-sfu-eng-kt; source ./oe-init-build-env; bitbake major-image'
+	build_path = SSHConnection.invoke_shell_command(yocto_path + cmd_bitbake)
+	print(build_path)
+
+def backup_image(SSHConnection):
+	if not os.path.exists('./tmp/' + glb_kt_ver):
+		os.makedirs('./tmp/' + glb_kt_ver)
+	remote_image_path = yocto_path.split()[-1].replace(';', '/') + 'build/tmp/deploy/images/saturn-sfu-eng-kt/'
+	print(remote_image_path)
+	image_file = [
+	'uImage',
+	'saturn_sfu.dtb',
+	'major-image-saturn-sfu-eng-kt-upgrade.kt.img',
+	'major-image-saturn-sfu-eng-kt_user.ubi',
+	'major-image-saturn-sfu-eng-kt.rootfs.squashfs.ubi'
+	]
+	local_image_path = './tmp/' + glb_kt_ver +'/'
+	for index, img in enumerate(image_file):
+		local_image = local_image_path + img
+		print(local_image)
+		remote_image = remote_image_path + img
+		print(remote_image)
+		SSHConnection.download(remote_image, local_image)
 
 if __name__ == "__main__":
 	info = read_ini()
@@ -158,12 +206,17 @@ if __name__ == "__main__":
 	p = int(info.get('port', None))
 	u = info.get('username', None)
 	pw = info.get('password', None)
+	# START connection
 	conn = SSHConnection(h, p, u, pw)
+	
 	# STEP 1. 
-	#yocto_gitclone(conn)
+	yocto_gitclone(conn) 
 	# STEP 2.
 	update_version(conn)
-	
+	# STEP 3.
+	build_image(conn)
+	# STEP 4.
+	backup_image(conn)
+
 	# CLOSE connection
 	conn.close()
-	
