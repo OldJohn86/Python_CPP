a9fd9e994b0775391fcf025e15bc57c1f1ab8a8f
Johnny Chen <johnchen5372@gmail.com>
2019-10-08 22:25:07 +0800

    update

--- cortina_task/kt_release/filter_c_file.py
+++ cortina_task/kt_release/filter_c_file.py
@@ -6,7 +6,19 @@
 import os
 from os import walk
 
-bhasCCommentBegin = False
+
+keywords = [
+    "#if "
+    "#ifdef",
+    "#ifndef",
+    "#if 0",
+    "#if defined",
+    "#if undefined"
+    "#if !defined"
+    "#elif",
+    "#endif"
+]
+
 
 def coding_verify(f_name):
     with open(f_name, 'rb') as f:
@@ -20,69 +32,39 @@ def coding_verify(f_name):
                     line.decode('utf8')
                 except:
                     print(str(line))
- 
+
+def parse_ifdef(lines):
+    length = len(lines)
+    print(length)
+    for i in range(0, length):
+        if "#if" in lines[i]:
+            templist = []
+            print('[' + str(i) + ']: ' + lines[i])
+            #templist.append(lines[i].split(' '))
+            #print(templist[0])
+            print(lines[i].lstrip(' ').rstrip('\n').split(' '))
+
 def filter_c_file(path):
     for root, dirs, files in os.walk(path):
         for f_name in files:
             if f_name.endswith('.c') or f_name.endswith('.h'):
-                global bhasCCommentBegin
-                print(os.path.join(root, f_name))
+                ff_name = os.path.join(root, f_name)
+                print(ff_name)
                 with open(os.path.join(root, f_name), 'r', encoding='utf-8', errors='ignore') as f:
                     lines = f.readlines()
-                length = len(lines)
-                print(length)
-                i = 0
-                ret = 0
-                while i < length:
-                    while True:
-                        if not bhasCCommentBegin:
-                            index = lines[i].find("/*")
-                            if index != -1:
-                                bhasCCommentBegin = True
-                                index2 = lines[i].find("*/", index + 2)
-                                if index2 != -1:
-                                    lines[i] = lines[i][0:index] + lines[i][index2 + 2:]
-                                    bhasCCommentBegin = False  # continue look for comment
-                                else:
-                                    lines[i] = lines[i][0:index]  # only find "begin comment
-                                    lines[i] += "\r\n"
-                                    ret = -2
-                                    break
-                            else:
-                                ret = 0  # not find
-                                break
-                        else:
-                            index2 = lines[i].find("*/")
-                            if index2 != -1:
-                                bhasCCommentBegin = False
-                                lines[i] = lines[i][index2 + 2:]  # continue look for comment
-                            else:
-                                ret = -1  # should delete this
-                                break
-                    if ret == -1:
-                        if bhasCCommentBegin == False:
-                            print("There must be some wrong")
-                        del lines[i]
-                        i -= 1
-                        length -= 1
-                    elif ret == 0:
-                        index = lines[i].find("//")
-                        if index != -1:
-                            lines[i] = lines[i][0:index]
-                            lines[i] += "\r\n"
-                        index = lines[i].find("#include")
-                        if index != -1:
-                            lines[i] = lines[i][0:index]
-                            lines[i] += "\r\n"
-                    else:
-                        pass
-                    i += 1
-                result = []
-                for line in lines:
-                    # line = re.sub (u'\t|\n| ','', line)
-                    line = line.replace('\n', '').replace('\t', '').replace(' ', '').replace('\r', '')
-                    # print(line,end='')
-                    result.append(line)
+                parse_ifdef(lines)
+
+#                length = len(lines)
+#                print(length)
+#                result = []
+#                i = 0
+#                while i < length:
+#                    for words in keywords:
+#                        if words in lines[i]:
+#                            print('[' + str(i) + ']: ' + lines[i])
+#                    i += 1
+                    #line = line.replace('\n', '').replace('\t', '').replace(' ', '').replace('\r', '')
+                    #result.append(line)
                 #f = open (path+filename.replace('.c', '.bak.c').replace ('.h', '.bak.h'), 'w')
                 #for i in result:  # 写文件
                 #    f.write (i)
